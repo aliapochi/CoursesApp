d441055906cab76965a6e627f4c63b68beac54d7
aliapochi <54737995+aliapochi@users.noreply.github.com>
2024-05-28 14:32:53 +0100

    Update MainActivity.kt

--- app/src/main/java/com/androidbasics/coursesapp/MainActivity.kt
+++ app/src/main/java/com/androidbasics/coursesapp/MainActivity.kt
@@ -45,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //CoursesApp("Android")
+                    CoursesApp("Android")
                 }
             }
         }
